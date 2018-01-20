9a7354dfa36a70fc14bb0264e8f0b9206c49d3de
JCIII <joseph.tallieu@gmail.com>
2018-01-20 17:02:33 -0600

    Improve setup instructions and correct some typos.  Also, had a duplicate code block on KeyboardInterrupt that no longer made sense to me

--- chomps/chomps.py
+++ chomps/chomps.py
@@ -40,63 +40,18 @@ READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
 
 # Load the plugins
 pwd = os.path.dirname(os.path.abspath(__file__))
-plugin_dir = os.path.join(pwd, "een_handlers")
+plugin_dir = os.path.join(pwd, settings.HANDLER_DIR)
 handlers = HandlerRegistry([plugin_dir], slack_client, BOT_NAME, BOT_ID)
 handlers.load_plugins()
+
 
 def simple_response(response, channel):
     slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
 
-def handle(handler, match, msg):
-    response = handler.process_message(match, msg)
-    if response:
-        simple_response(response, msg['channel'])
 
 if __name__ == "__main__":
-    try:
-        if slack_client.rtm_connect():
-            print("Chomps: connected and running!")
-            while True:
-                rtm = slack_client.rtm_read()
-                if rtm and len(rtm) > 0:
-                    try:
-                        # For each message read - do the NEW HANDLERS
-                        for msg in rtm:
-                            # Do not respond to message from ourselves
-                            if msg.get('user') == BOT_ID:
-                                continue
-
-                            text = msg.get('text', '')
-                            if text:
-                                print "CHANNEL: {} => Message: {}".format(msg['channel'], text.encode('ascii', 'ignore'))
-                                for handler in handlers:
-                                    count = 0
-                                    for match in handler.pattern.finditer(text):
-                                        count += 1
-                                        if count <= handler.call_limit:
-                                            #gevent.spawn(handle, handler, match, msg)
-                                            response = handler.process_message(match, msg)
-                                            if response:
-                                                simple_response(response, msg['channel'])
-                                        else:
-                                            break
-
-                    except Exception as e:
-                        print("Error {} {}".format(type(e), e.message))
-                        traceback.print_exc()
-
-                time.sleep(READ_WEBSOCKET_DELAY)
-        else:
-            print("Connection failed. Invalid Slack token or bot ID?")
-    except KeyboardInterrupt:
-        # Clean up greenlets
-        import gc
-        from greenlet import greenlet
-        try:
-            gevent.killall([obj for obj in gc.get_objects() if isinstance(obj, greenlet)])
-        except:
-            pass
-
+    if slack_client.rtm_connect():
+        print("Chomps: connected and running!")
         while True:
             rtm = slack_client.rtm_read()
             if rtm and len(rtm) > 0:
@@ -116,11 +71,9 @@ if __name__ == "__main__":
                                     count += 1
                                     if count <= handler.call_limit:
                                         #gevent.spawn(handle, handler, match, msg)
-                                        
                                         response = handler.process_message(match, msg)
                                         if response:
                                             simple_response(response, msg['channel'])
-                                        
                                     else:
                                         break
 
@@ -131,4 +84,3 @@ if __name__ == "__main__":
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
-
--- chomps/handlers/my_handler.py
+++ chomps/handlers/my_handler.py
@@ -1,25 +1,23 @@
 
 import re
-import random
-import requests
-import simplejson
 from pprint import pprint
 from lib import ChompsHandler
 
 
-##### This is where the part starts - The BotHandler ####
+# ---- This is where the party starts - The BotHandler -----
 # Essentially you are defining what you trigger on and the method to generate the
 # response
 
 # define what you wanna trigger on and what you want matched in groups.
 trigger = re.compile('chomps marco')
 
-class MyHandler(ChompsHandler):    
+
+class MyHandler(ChompsHandler):
     @property
     def pattern(self):
         """This is called by the chomps engine to get the pattern for the function"""
         return trigger
-        
+
     def process_message(self, match, msg):
         pprint(msg)
         return "*POLO*"
@@ -30,9 +28,9 @@ if __name__ == "__main__":
     namer = MyHandler(None, None, None)
 
     tests = [
-        ("chomps marco",1),
+        ("chomps marco", 1),
         ("chomps name", 0),
-        ("marco",0),
+        ("marco", 0),
     ]
 
     for test in tests:
@@ -48,6 +46,3 @@ if __name__ == "__main__":
             print "\tPASSED"
         else:
             print "\tFALSE"
-
-
-
